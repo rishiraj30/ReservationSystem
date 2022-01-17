b4bbf450468aae16a471aca6490d43c4cbeca392
Vasu Aggarwal <svasu.sv95@gmail.com>
2022-01-18 00:14:08 +0530

    Added 2 new intents: 1. RegisterUser.kt 2. ViewUser.kt and completed the design of activity_register_user.xml

--- app/src/main/java/com/example/reservationsystem/MainActivity.kt
+++ app/src/main/java/com/example/reservationsystem/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.reservationsystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        registerbutton.setOnClickListener{
+            var reintent = Intent(this, RegisterUser::class.java)
+            startActivity(reintent)
+        }
+
+        vubutton.setOnClickListener {
+            var newintent = Intent(this, ViewUser::class.java)
+            startActivity(newintent)
+        }
     }
 }
